8305771ac0f628ab655fd2d67a59f35d06636db6
vm <vm@Unknown>
2021-08-21 00:42:01 +0200

    duracel

--- Nhala/src/nhala/Kendze.java
+++ Nhala/src/nhala/Kendze.java
@@ -1,17 +1,59 @@
 package nhala;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+import java.io.*;
 
-/**
- *
- * @author kiDDing
- */
 public class Kendze {
-    public static void main(String []args){
-        System.out.println("Hello World!");
+    public static void main(String []args) throws IOException {
+        BufferedReader ler = new BufferedReader(new InputStreamReader(System.in));
+        byte op, op1;
+        
+        
+        do {
+            System.out.println("1. Mercados");
+            System.out.println("2. Visualizar Dados de Mercados.");
+            System.out.println("0. Sair");
+            System.out.println("Opcao: ");
+            do {
+                op = Byte.parseByte(ler.readLine());
+                
+                if(op < 0 || op > 2)
+                    System.out.println("Informe correctamente a opcao.");
+            }while(op < 0 || op > 2);
+            
+            switch(op) {
+                case 1:
+                    
+                    System.out.println("1. Mercado Formal");
+                    System.out.println("2. Mercado Informal");
+                    System.out.println("3. Mercado Semi-Informal");
+                    System.out.println("Opcao: ");
+                    do {
+                        op1 = Byte.parseByte(ler.readLine());
+
+                        if(op1 < 0 || op1 > 3)
+                            System.out.println("Informe correctamente a opcao.");
+                    }while(op1 < 0 || op1 > 3);
+                    
+                    switch(op1) {
+                        case 1:
+                            
+                        break;
+                        case 2:
+                            
+                        break;
+                        case 3:
+                            
+                        break;
+                    }
+                    
+                    
+                break;
+                
+                case 2:
+                    
+                break;
+            }
+            
+        }while(op != 0);
     }
 }
